035eabead404fb60ea1a2dc34f447b4b91393b83
Erick Escobar <edeajob27@gmail.com>
2022-08-17 09:39:50 -0500

    Declaración de los primeros objetos

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,16 @@
+
+#! Se tiene que importar la clase desde el archivo en donde se encuentra
+from car import Car
+#* Entrypoint normal para cualquier programa de python 
 if __name__ == "__main__":
-    print("Running command")
+    #* Declaración de los objetos car y car2
+    car = Car()
+    car.license = "MIT890"
+    car.driver = "Erick"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "MIT890"
+    car2.driver = "Edwin"
+    print(vars(car2))
+
