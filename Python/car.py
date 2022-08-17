bfd7deca5ce63336a015b41a1807bbff0bd07d7c
Erick Escobar <edeajob27@gmail.com>
2022-08-17 10:24:28 -0500

    Cree el constructor de car y definimos las variables obligatorias

--- Python/car.py
+++ Python/car.py
@@ -3,3 +3,6 @@ class Car:
     license = str
     driver = str
     passenger = int
+    def __init__(self,  license, driver):
+        self.license = license
+        self.driver = driver
--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,19 @@
 
 #! Se tiene que importar la clase desde el archivo en donde se encuentra
 from car import Car
+from account import Account
 #* Entrypoint normal para cualquier programa de python 
 if __name__ == "__main__":
-    #* Declaración de los objetos car y car2
-    car = Car()
-    car.license = "MIT890"
-    car.driver = "Erick"
+    #* Declaración del objeto car
+    '''
+    Recuerda que car recibe la placa y el conductor
+    Account recibe nombre y documento del objeto sea usuario o..
+    Sea conductor en este caso debido a la composicion de car
+    Estamos hablando del conductor.
+    Por ultimo el objeto car se crea con su respectiva placa y conductor.
+    '''
+    car = Car("AMZ456",Account("Erick","5465643"))
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "MIT890"
-    car2.driver = "Edwin"
-    print(vars(car2))
 
